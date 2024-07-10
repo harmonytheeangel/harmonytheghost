cb9c4a4bba73fc07a5ec4f0c53e07d01505a77d9
harmonytheeangel <harmonyiremitide@gmail.com>
2024-07-10 08:23:56 +0100

    Add files via upload

---
+++ Amni_food/hello_world.ts
@@ -0,0 +1,12 @@
+let username = "Harmony";
+console.log("hi");
+let num = 1;
+const json = JSON.parse("55");
+
+console.log(typeof(json))
+if (num == 0){
+    console.log("null")
+}
+else{
+    console.log("Harmony:@harmonytheeangel")
+}
---
+++ Amni_food/pages/menu.js
@@ -0,0 +1,5 @@
+document.querySelectorAll("button")[1].addEventListener("click", clicked);
+
+function clicked() {
+  alert(9);
+}
